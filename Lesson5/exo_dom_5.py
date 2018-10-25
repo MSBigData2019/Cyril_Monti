5de1c6b5926e1c77422616d0c45b89055bfe8c42
cyrmonti <cyril.monti@telecom.paristech.fr>
2018-10-25 17:20:06 +0200

    crawler lacentrale .py

---
+++ Lesson5/exo_dom_5.py
@@ -0,0 +1,83 @@
+from requests import get
+from bs4 import BeautifulSoup
+import math
+import pandas as pd
+
+
+def get_url(url):
+    url = url
+    response = get(url)
+    soup = BeautifulSoup(response.text, 'html.parser')
+    return(soup)
+
+
+def get_links(soup):
+    liste_annonce = []
+    for annonce in soup.find_all('div', class_="adContainer"):
+        for an in annonce.find_all('a'):
+            liste_annonce.append(an['href'])
+    return liste_annonce
+
+
+def get_all_pages():
+    soup = get_url("https://www.lacentrale.fr/listing?makesModelsCommercialNames=RENAULT%3AZOE&regions=FR-PAC%2CFR-IDF%2CFR-NAQ")
+    nb_annonce_total = int(soup.find('span', class_='numAnn').text.strip())
+    nb_annonce_page = 16
+    nb_page = math.ceil(nb_annonce_total / nb_annonce_page)
+    liste = []
+    for page in range(nb_page):
+        soup = get_url("https://www.lacentrale.fr/listing?makesModelsCommercialNames=RENAULT%3AZOE&options=&page={}&regions=FR-PAC%2CFR-IDF%2CFR-NAQ"
+                       .format(page))
+        lien = get_links(soup)
+        liste.append(lien)
+    return liste
+
+
+def flatten(liens):
+    liste_lien = []
+    for tableau in liens:
+        for lien in tableau:
+            liste_lien.append(lien)
+    return liste_lien
+
+
+def remove_duplicates(l):
+    return list(set(l))
+
+
+def get_info(urls):
+    modeles = []
+    telephones = []
+    annees = []
+    kilometres = []
+    vendeurs = []
+    prices = []
+    for url in urls:
+        soup = get_url("https://www.lacentrale.fr{}".format(url))
+        modele = soup.find('div', class_='versionTxt txtGrey7C sizeC mB10 hiddenPhone').text.strip()
+        telephone = soup.find('div', class_='phoneNumber1').text.strip()
+        annee = soup.find('span', class_='clearPhone lH35').text.strip()
+        kilometre = soup.find('span', class_='clearPhone lH40').text.strip()
+        vendeur = soup.find('div', class_='bold italic mB10').text[40:58].strip()
+        prix = soup.find('strong', class_='sizeD lH35 inlineBlock vMiddle ').text.strip()
+        modeles.append(modele)
+        telephones.append(telephone)
+        annees.append(annee)
+        kilometres.append(kilometre)
+        vendeurs.append(vendeur)
+        prices.append(prix)
+    dictionnaire = dictionnaire = {"modeles":modeles,
+                    "telephones": telephones,
+                    "annee":annees,
+                    "kilometres":kilometres,
+                    "vendeurs":vendeurs,
+                    "prix":prices}
+    return dictionnaire
+
+
+liens = get_all_pages()
+lien_flattened = flatten(liens)
+lien_f = remove_duplicates(lien_flattened)
+dictionnaire = get_info(lien_f)
+df = pd.DataFrame(dictionnaire)
+print(df)
